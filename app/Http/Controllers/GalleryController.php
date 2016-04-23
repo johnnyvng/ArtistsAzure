e2fa387374e56bedcf87fd97af5d4e171b9f9eaa
Johnny <nguyen.johnny@live.com>
2016-04-23 15:58:32 -0400

    restore

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -17,11 +15,10 @@ class GalleryController extends Controller
     //
     public function index ($id = 1)
     {
-    	// return Gallery::all();
-    	return Gallery::where('user_id', Auth::user()->id)->get();
-        // $galleries = Gallery::all();
-        // $gallery = Gallery::find($id);
-        // return view('gallery', ['gallery' => $gallery, 'all_galleries' => $galleries]);
+
+        $galleries = Gallery::all();
+        $gallery = Gallery::find($id);
+        return view('gallery', ['gallery' => $gallery, 'all_galleries' => $galleries]);
         
     }
 }
